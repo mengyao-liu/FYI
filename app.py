9d87315989f58cfd9d8eed28eb275724e75018e6
Mengyao <mengyao.cecily.liu@gmail.com>
2021-02-18 00:11:41 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,121 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import SessionState
+import dill
+import json
+from PIL import Image
+import re
+import fig_wc
+
+
+
+
+def pageZero(sesh, merchant):
+    st.header('Stay tuned...')
+
+
+def pageOne(sesh, merchant):
+    with open(merchant+'/ins_'+merchant+'_top50_3months_rank.dill', 'rb') as in_strm:
+        df_score = dill.load(in_strm) 
+    d = df_score.reset_index().rename({'username':'Influencer', 'score':'Score'}, axis='columns')
+    d.loc[:,'Score'] = ['%.2f'%i for i in d['Score']]
+    d.loc[:,'Influencer'] = ['@'+i for i in d['Influencer']]
+    d.index  += 1
+    with open(merchant+'/chart_'+merchant+'.json', "rb") as f:
+        chart = json.load(f)
+    
+
+    if not sesh.button_sent:
+        st.balloons()
+    st.header('Here are the top 5 influencers we recommend for you!')
+    st.markdown('###')
+    st.write(d[:5])
+    st.write('Out of *375* active verified influencers with No. of followers 100 - 1k')
+    
+    st.markdown('#')    
+    st.header('Check out the key metrics.')
+    st.markdown('###')
+    st.vega_lite_chart(chart, width=800, height=400)
+    
+    st.markdown('#') 
+    st.header("Check out what **your influencer's** ***followers*** are talking about recently.")
+    show_wc(merchant)
+    
+          
+def show_wc(merchant):
+    ins = st.text_input('Influencer', )
+    insname = re.sub('[\s]*[@]*', '', ins)
+    if len(insname)>0:
+        try:
+            image = Image.open(merchant+'/wc/'+insname+'_followers_tweetstext.png')
+            st.image(image, caption='Word cloud of '+insname+"'s followers in their recent 30 tweets")
+        except:
+            with st.spinner("Generating the word cloud..."):
+                image = fig_wc.plot(merchant, insname)
+                st.pyplot(image, caption='Word cloud of '+insname+"'s followers in their recent 30 tweets")
+        #else:
+            #image = Image.open('bird.png')
+            #st.image(image, caption='Not available yet')
+        
+    
+
+def pageTwo(sesh, merchant):
+    st.header('No match found. Try another influencer type!')
+    
+    
+sesh = SessionState.get(curr_page = 0, curr_index=0, last_index=-1, button_sent=False)
+PAGES = [pageZero, pageOne, pageTwo]
+#st.set_page_config(layout="wide")
+
+def main():
+    #####MAIN PAGE NAV BAR:
+    st.markdown(' # Find Your Influencer')
+    
+    st.markdown(' ## Step 1: Please select your product category')
+    cat_list = ['Books 📖','Beauty 💄','Sports 🏸','Clothing 👕','Electronics 🖥']
+    cat = st.selectbox('', cat_list)
+    loc = {'Books 📖':'book', 'Beauty 💄': 'beauty', 'Sports 🏸':'sports', 'Clothing 👕':'clothing', 'Electronics 🖥':'electronics'}
+    
+    st.markdown(' ## Step 2: Please select your influencer type')
+    inscat_list = ['100 - 1k 💰', '1k - 10k 💰💰', '10k - 100k 💰💰💰', '>100k 💰💰💰💰']
+    inscat = st.selectbox("No. of followers", inscat_list)
+    
+    sesh.curr_index = cat_list.index(cat) + 10*inscat_list.index(inscat)
+    st.markdown('##')
+    
+    
+    
+    if inscat=='100 - 1k 💰':
+        if cat=='Electronics 🖥':
+            sesh.curr_page = 2
+        else:
+            sesh.curr_page = 1
+    else:
+        sesh.curr_page = 0
+        
+        
+    if sesh.curr_index!=sesh.last_index:
+        sesh.button_sent=False
+        sesh.last_index = sesh.curr_index
+    
+    
+    if st.button('Submit'):    
+        with st.spinner("Getting your influencers..."):
+            st.markdown('##')            
+            page_turning_function = PAGES[sesh.curr_page]
+            page_turning_function(sesh, loc[cat])
+            sesh.button_sent = True
+            
+            
+    elif sesh.button_sent: 
+        st.markdown('##') 
+        page_turning_function = PAGES[sesh.curr_page]
+        page_turning_function(sesh, loc[cat])
+        
+            
+    
+
+if __name__=='__main__':
+    main()
---
+++ fig_wc.py
@@ -0,0 +1,39 @@
+import dill
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from PIL import Image
+from wordcloud import WordCloud
+from nltk.corpus import stopwords
+import re
+
+def plot(merchant, ins):
+    with open(merchant+'/ins_'+merchant+'_top50_followers_30tweets.dill', 'rb') as in_strm:
+        d_followers_tweetstext = dill.load(in_strm)  
+
+    text = ' '.join([re.sub('(https://[\w\./]*)', ' ', re.sub('(@[\w]*)', ' ', i)) for i in d_followers_tweetstext[ins].values()])
+
+
+    def transform_format(val):
+        if val == 0:
+            return 255
+        else:
+            return 88
+
+    bird_mask = np.array(Image.open("bird.png"))
+    transformed_bird_mask = np.ndarray((bird_mask.shape[0],bird_mask.shape[1]), np.int32)
+    for i in range(len(bird_mask)):
+        transformed_bird_mask[i] = list(map(transform_format, bird_mask[i]))    
+
+
+
+    stop_words = set(stopwords.words('english')+['https','RT','amp','u','one',"I'm"])
+    wordcloud = WordCloud(stopwords=stop_words, max_words=30, background_color="white", mask=transformed_bird_mask, contour_width=2, contour_color='lightblue',repeat=True).generate(text)
+
+
+    fig = plt.figure()
+    plt.imshow(wordcloud, interpolation="bilinear")
+    plt.axis("off")
+    #plt.show()
+    #wordcloud.to_file(merchant+'/wc/'+ins+"_followers_tweetstext.png")
+    return fig
